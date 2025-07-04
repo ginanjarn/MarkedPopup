18ffb72a7d0f1f1402a3b813a5f2ea53f5c427a9
ginanjarn <ginanjarn@gmail.com>
2025-07-04 17:35:47 +0700

    lazy import renderer module

--- renderer.py
+++ renderer.py
@@ -8,15 +8,16 @@ LIBRARY_PATH = str(Path(__file__).parent.joinpath("library"))
 if LIBRARY_PATH not in sys.path:
     sys.path.append(LIBRARY_PATH)
 
-import mistletoe
-import docutils.core as docutils_core
-
 
 def render_markdown(text: str) -> str:
     """render markdown script"""
+    import mistletoe
+
     return mistletoe.markdown(text)
 
 
 def render_rst(text: str) -> str:
     """render rst script"""
+    import docutils.core as docutils_core
+
     return docutils_core.publish_parts(text, writer_name="html")["html_body"]
